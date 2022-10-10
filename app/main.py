2775d4a3d3b3362a661ff0e448c3635aaf1a1f6f
wanliao <wanyuliao4@gmail.com>
2022-10-10 12:34:18 +0800

    feat: tooltips & data table

--- app/main.py
+++ app/main.py
@@ -1,60 +1,87 @@
 # main.py
 
-import pandas as pd
-from os.path import dirname, join
+# todo:
+#   - add annual/weekly (RadioButtonGroup)
+#   - add filter
 
 from bokeh.io import curdoc
 from bokeh.layouts import column, row
-from bokeh.models import ColumnDataSource, Div, Slider
-from bokeh.plotting import figure, show
+from bokeh.models import ColumnDataSource, Div, Slider, DataTable, TableColumn, DateFormatter
+from bokeh.plotting import figure
+import pandas as pd
 
 # import dataset
 df = pd.read_csv('./data/box_office_dataset.csv')
 
 # dataset preprocessing
-df['統計起始日'] = pd.to_datetime(df['統計起始日'])
-df['上映日期'] = pd.to_datetime(df['上映日期'])
-df['累計銷售票數'] = pd.to_numeric(df['累計銷售票數'].str.replace(',', '')).astype('Int64')
-df['累計銷售金額'] = pd.to_numeric(df['累計銷售金額'].str.replace(',', '')).astype('Float64')
-df['累計銷售金額_萬'] = df['累計銷售金額'].div(10000)
-null_index = df.index[(df['累計銷售票數'].isnull()) | (df['累計銷售金額'].isnull())]
+df.rename(columns = { '統計起始日': 'date', '上映日期': 'release', '上映院數': 'theaters', '中文片名': 'name', '國別地區': 'country', '累計銷售票數': 'tickets', '累計銷售金額': 'revenue' }, inplace = True)
+df['date'] = pd.to_datetime(df['date'])
+df['release'] = pd.to_datetime(df['release'])
+df['tickets'] = pd.to_numeric(df['tickets'].str.replace(',', '')).astype('Int64')
+df['revenue'] = pd.to_numeric(df['revenue'].str.replace(',', '')).astype('Float64')
+df['revenue_10k'] = df['revenue'].div(10000)
+null_index = df.index[(df['tickets'].isnull()) | (df['revenue_10k'].isnull())]
 df = df.drop(null_index)
 
-source = ColumnDataSource(data = dict(上映日期 = [], 上映院數 = [], 中文片名 = [], 國別地區 = [], 累計銷售票數 = [], 累計銷售金額 = [], 累計銷售金額_萬 = []))
+source = ColumnDataSource(data = dict(date = [], release = [], theaters = [], name = [], country = [], tickets = [], revenue = [], revenue_10k = []))
 
 # functions
 def selected():
     # todo: add filters
     ticket_val = ticket_slider.value
-    selected = df[df['累計銷售票數'] >= ticket_val]
+    selected = df[df['tickets'] >= ticket_val]
     return selected
 
 def callback():
     # todo: add filters
     df = selected()
-    p.title.text = '%d movies selected' % len(df)
+    plot.title.text = '%d movies selected' % len(df)
     source.data = dict(
-        累計銷售票數 = df['累計銷售票數'],
-        累計銷售金額 = df['累計銷售金額']
+        date = df['date'],
+        release = df['release'],
+        theaters = df['theaters'],
+        name = df['name'],
+        country = df['country'],
+        tickets = df['tickets'],
+        revenue = df['revenue'],
+        revenue_10k = df['revenue_10k']
     )
 
 # widget
 # todo: add filters
-ticket_slider = Slider(start = 0, end = max(df['累計銷售票數']), value = 0, step = 100000, title = '累計銷售票數大於')
+ticket_slider = Slider(start = 0, end = max(df['tickets']), value = 0, step = 100000, title = 'Tickets sold greater than')
 ticket_slider.on_change('value', lambda attr, old, new: callback())
 
 # plot
-p = figure(width = 600, height = 350, toolbar_location = None, sizing_mode = 'scale_both', title = '')
-p.circle(x = '累計銷售票數', y = '累計銷售金額', source = source)
+tooltips = [
+    ('Movie', '@name'),
+    ('Tickets', '@tickets'),
+    ('NT$', '@revenue')
+]
+plot = figure(width = 850, height = 280, tooltips = tooltips, toolbar_location = None, sizing_mode = 'scale_both', title = '')
+plot.circle(x = 'tickets', y = 'revenue', source = source, size = 5)
+
+# data table
+columns = [
+    TableColumn(field = 'date', title = 'Statistic date', formatter = DateFormatter()),
+    TableColumn(field = 'release', title = 'Released date', formatter = DateFormatter()),
+    TableColumn(field = 'theaters', title = 'Theaters'),
+    TableColumn(field = 'name', title = 'Movie'),
+    TableColumn(field = 'country', title = 'Country'),
+    TableColumn(field = 'tickets', title = 'Tickets'),
+    TableColumn(field = 'revenue', title = 'NT$')
+]
+datatable = DataTable(width = 850, height = 280, source = source, columns = columns, sortable = True)
 
 # html
 index = Div(text = open('./app/index.html').read(), sizing_mode = 'stretch_width')
 footer = Div(text = open('./app/footer.html').read(), sizing_mode = 'stretch_width')
 # todo: add filters
-filters = column(ticket_slider, width = 320)
+filters = column(ticket_slider)
+results = column(plot, datatable)
 
-l = column(row(index), row(filters, p), row(footer), sizing_mode = "scale_both")
+layout = column(row(index), row(filters, results), row(footer), sizing_mode = 'scale_both')
 callback()
 
-curdoc().add_root(l)
-curdoc().title = "Box office TW"
+curdoc().add_root(layout)
+curdoc().title = 'Box office TW'
